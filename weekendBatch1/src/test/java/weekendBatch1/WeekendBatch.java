00d6a22766ae9d59bd1bfee9c4e6ee831d7c5c13
Amar2595 <Chn251995@gmail.com>
2022-08-20 16:13:52 +0530

    WeekendBatch

---
+++ weekendBatch1/src/test/java/weekendBatch1/WeekendBatch.java
@@ -0,0 +1,10 @@
+package weekendBatch1;
+
+public class WeekendBatch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welecome To Weekend Batch");
+	}
+
+}
